44029bc672d5a66fcb1e8c3575184c63bc4aae51
TommyOves <TommyOves@Tommys-MacBook-Air.local>
2022-03-16 22:09:35 -0400

    Finished first 3 components

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,26 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    function flipType() {
+        if (type === "short_answer_question") {
+            setType("multiple_choice_question");
+        } else {
+            setType("short_answer_question");
+        }
+    }
+    function returnType(given_type: QuestionType) {
+        if (given_type === "short_answer_question") {
+            return "Short Answer";
+        } else {
+            return "Multiple Choice";
+        }
+    }
+    return (
+        <div>
+            <span>{returnType(type)}</span>
+            <Button onClick={flipType}>Change Type</Button>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,34 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+    function startFunction() {
+        setProgress(!progress);
+        setAttempts(attempts - 1);
+    }
+    return (
+        <div>
+            <div>
+                Attempts: <span>{attempts}</span>
+            </div>
+            <div>
+                <Button
+                    onClick={startFunction}
+                    disabled={progress || attempts === 0}
+                >
+                    Start
+                </Button>
+                <Button onClick={() => setProgress(false)} disabled={!progress}>
+                    Stop
+                </Button>
+                <Button
+                    onClick={() => setAttempts(1 + attempts)}
+                    disabled={progress}
+                >
+                    Mulligan
+                </Button>
+            </div>
+        </div>
+    );
 }
